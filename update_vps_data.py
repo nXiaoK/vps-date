af0a5e777849d33745632067ba855bcd9a205aab
woniu336 <310478073@qq.com>
2024-12-01 13:21:18 +0800

    Update My Site

--- update_vps_data.py
+++ update_vps_data.py
@@ -63,7 +63,7 @@ def get_vps_data():
             "name": "斯巴达VPS",
             "cost": 8,
             "currency": "USD",
-            "monthlyExpireDay": 15,
+            "monthlyExpireDay": 3,
             "color": "primary",
             "url": "https://billing.spartanhost.net/login"
         },
